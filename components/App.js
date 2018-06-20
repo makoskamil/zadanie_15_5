89eecfc0622f45cfa4f740b52cb1a4ce09095e07
Kamil Makoś <makos.kamil@o2.pl>
2018-06-20 21:52:56 +0200

    add_promise

--- components/App.js
+++ components/App.js
@@ -40,21 +40,28 @@ App = React.createClass({
         xhr.send();
     },
     */
-        getGif: function(searchingText, callback) {
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data;
-                var gif = {
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
+        getGif: function(searchingText){
+            
+            return new Promise(
+            function(resolve){  //
+                
+                var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+                var xhr = new XMLHttpRequest();
+            
+                xhr.open('GET', url);
+                xhr.onload = function() {
+                    
+                    if (xhr.status === 200) {
+                        var data = JSON.parse(xhr.responseText).data;
+                        var gif = {
+                            url: data.fixed_width_downsampled_url,
+                            sourceUrl: data.url
+                        };
+                        resolve(gif);
+                    }
                 };
-                callback(gif);
-            }
-        };
-        xhr.send();
+                xhr.send();
+            });
     },
     
     render: function() {
